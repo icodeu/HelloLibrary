ccc92f3d5f1c7efa2d5cbf60bce2937c7b21007c
icodeu <icodeyou@sina.com>
2015-12-23 21:03:50 +0800

    init

---
+++ app/src/main/java/com/icodeyou/hellolibrary/MainActivity.java
@@ -0,0 +1,23 @@
+package com.icodeyou.hellolibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.icodeyou.library.CustomUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Log.d(TAG, "library say : " + CustomUtil.getMyHero());
+    }
+}
---
+++ library/src/main/java/com/icodeyou/library/CustomUtil.java
@@ -0,0 +1,12 @@
+package com.icodeyou.library;
+
+/**
+ * Created by wanghuan on 15/12/23.
+ */
+public class CustomUtil {
+
+    public static String getMyHero() {
+        return "icodeyou";
+    }
+
+}
